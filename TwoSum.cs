74ae3b487278a50df93fead3b118eb936848947b
Ricardo Rivera Pérez <43622894+Ricardo55010@users.noreply.github.com>
2023-01-30 18:22:06 -0600

    Update TwoSum.cs

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,1 +1,16 @@
 
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var output = new int [2];
+        for(int i=0;i<nums.Length;i++){ //i loop for first pointer
+            for(int j=i+1;j<nums.Length;j++){ //j loop for second pointer 
+                if(nums[i]+nums[j]==target){
+                    output[0]=i;
+                    output[1]=j;;
+                    
+                }
+            }
+        }
+        return output;
+    }
+}

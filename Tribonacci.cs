e68e2c0d6d34e810400224806f33392934059bf0
Ricardo Rivera Pérez <43622894+Ricardo55010@users.noreply.github.com>
2023-02-01 17:02:13 -0600

    Create Tribonacci.cs

---
+++ Tribonacci.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        List <int> TribonacciSeries = new List <int>();
+        int tribonacciTotal=1;
+        TribonacciSeries.Add(0);
+        TribonacciSeries.Add(1);
+        TribonacciSeries.Add(1);
+        for(int i=3;i<=n;i++){
+            TribonacciSeries.Add(TribonacciSeries[i-3]+TribonacciSeries[i-2]+TribonacciSeries[i-1]);
+        }
+        return TribonacciSeries[n];
+    }
+}
